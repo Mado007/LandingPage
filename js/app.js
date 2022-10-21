89940abe5ee0e662827725ab9c84a19dabd2f546
Mahmoud.Eid <39015047+Mado007@users.noreply.github.com>
2022-10-22 00:43:21 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -21,7 +21,8 @@
  * Define Global Variables
  * 
 */
-
+const sections = document.querySelectorAll('section');
+const navBarList = document.getElementById("navbar__list");
 
 /**
  * End Global Variables
@@ -38,13 +39,33 @@
 */
 
 // build the nav
-
-
+function buildNav(){
+  const fr = document.createDocumentFragment();
+  for (let sec of sections) {
+      const li = document.createElement('li');
+      const anchor = document.createElement('a');
+      //no need for the href attribute.
+      anchor.className = "menu__link";
+      anchor.setAttribute('data-id', sec.id);
+      anchor.textContent = sec.getAttribute('data-nav');
+      li.appendChild(anchor);
+      fr.appendChild(li);
+  }
+  navBarList.appendChild(fr);
+}
+buildNav();
 // Add class 'active' to section when near top of viewport
 
 
 // Scroll to anchor ID using scrollTO event
-
+function scroll(evt) {
+  const link = evt.target;
+  if (link.nodeName == 'A') {
+      evt.preventDefault();
+      const e = document.getElementById(link.getAttribute('data-id'));
+      e.scrollIntoView({behavior:"smooth" , block: 'center'});
+  }
+}
 
 /**
  * End Main Functions
@@ -53,17 +74,17 @@
 */
 
 // Build menu 
-
 // Scroll to section on link click
-
+navBarList.addEventListener('click', scroll);
 //smooth scrolling
 document.getElementById('navbar__list').addEventListener('click', function (e) { e.preventDefault();
-    const target = e.target;
-    if (target.classList.contains('menu__link')) {
-        const id = target.getAttribute('href').slice(1);
-        document.getElementById(id).scrollIntoView({ behavior: 'smooth',block: "center" });
-    }
+  const target = e.target;
+  if (target.classList.contains('menu__link')) {
+      const id = target.getAttribute('href').slice(1);
+      document.getElementById(id).scrollIntoView({ behavior: 'smooth'});
+  }
 });
-
+// Set sections as active
+window.addEventListener("scroll", scrollHandler);
 
           
